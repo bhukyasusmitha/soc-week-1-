25c25caedd07ec761d4e506c9cbb599301d745c8
bhukyasusmitha <susmithainfinity@gmail.com>
2025-06-27 23:29:30 +0530

    Update w3_q3.cpp

--- week_3/w3_q3.cpp
+++ week_3/w3_q3.cpp
@@ -1,35 +1,22 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 
 const int MOD = 1000000007;
 
 int main() {
-    int num;
-    cin >> num;  
+int num ;
+cin >> num ; 
+vector<int> ways(num + 1, 0);
+ways[0] = 1;
+for (int total = 1; total <= num; total++) {
+    for (int dice = 1; dice <= 6; dice++) {
+ if (total - dice >= 0) {
+ways[total] = (ways[total] + ways[total - dice]) % MOD;
+    }}
+    }
+cout << ways[num] << endl;
 
    
-    vector<int> w(num + 1, 0);
-
- 
-    w[0] = 1;
-
-
-    for (int  = 1; total <= num; total++) {
-        
-        for (int dice = 1; dice <= 6; dice++) {
-    
-            if (total - dice >= 0) {
-    
-                w[total] = (w[total] + w[total - dice]) % MOD;
-            }
-        }
-    }
-
-    
-    cout << w[num] << endl;
-
-    return 0;
 }
