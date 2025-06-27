3d6ec857758213d8a144f99f613c1f8b531e73cf
bhukyasusmitha <susmithainfinity@gmail.com>
2025-06-27 23:28:38 +0530

    Update w3_q1.cpp

--- week_3/w3_q1.cpp
+++ week_3/w3_q1.cpp
@@ -5,36 +5,20 @@
 using namespace std;
 
 int main() {
-    int num;
-    cin >> num;
+int num;
+cin >> num;
+vector<pair<int, int>> tasks;
+for (int i = 0; i < num; i++) {
+int a, d;
+ cin >> a >> d;
+ tasks.push_back({a, d});}
+sort(tasks.begin(), tasks.end());
+long long c_time = 0;   
+long long t_reward = 0;   
+for (int i = 0; i < num; i++) {
+int duration = tasks[i].first;
+int deadline = tasks[i].second;
+c_time += duration;t_reward += (deadline - c_time);  }
 
-    
-    vector<pair<int, int>> tasks;
-
-   
-    for (int i = 0; i < num; i++) {
-        int a, d;
-        cin >> a >> d;
-        tasks.push_back({a, d});
-    }
-
-    
-    sort(tasks.begin(), tasks.end());
-
-    long long c_time = 0;   
-    long long t_reward = 0;   
-
-
-    for (int i = 0; i < num; i++) {
-        int duration = tasks[i].first;
-        int deadline = tasks[i].second;
-
-        c_time += duration;          
-        t_reward += (deadline - c_time);  
-    }
-
-
-    cout << t_reward << endl;
-
-    return 0;
+cout << t_reward << endl;
 }
